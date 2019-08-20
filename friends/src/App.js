37f89c34eda2f9c940da271a714bffdbc90c0d12
Tommy <tommykindle83@gmail.com>
2019-08-20 16:13:45 -0700

    Added functionality to get friends list from API finished MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import PrivateRoute from './components/PrivateRoute'
+import FriendsList from './components/FriendsList'
 import './App.css';
 
 function App() {
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -25,6 +25,7 @@ class Login extends React.Component {
         localStorage.setItem('token', res.data.payload)
       })
       .catch(err => console.log("error", err))
+    this.props.history.push('/friends')
   }
 
   render() {
